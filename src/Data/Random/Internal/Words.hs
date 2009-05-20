3eb0e2241492bbc01ad42c20f62f7c37e56fd946
James Cook <james.cook@usma.edu>
2009-05-20 17:15:39 +0000

    Cleaned up and commented Data.Random.Internal.Words

--- src/Data/Random/Internal/Words.hs
+++ src/Data/Random/Internal/Words.hs
@@ -3,9 +3,6 @@
  -}
 
 -- |A few little functions I found myself writing inline over and over again.
---
--- Note that these need to be checked to ensure proper behavior on big-endian 
--- systems.  They are probably not right at the moment.
 module Data.Random.Internal.Words where
 
 import Foreign
@@ -19,21 +16,25 @@ import Control.Monad
 buildWord :: Word8 -> Word8 -> Word8 -> Word8 -> Word8 -> Word8 -> Word8 -> Word8 -> Word64
 buildWord b0 b1 b2 b3 b4 b5 b6 b7
     = unsafePerformIO . allocaBytes 8 $ \p -> do
-        pokeElemOff p 0 b0
-        pokeElemOff p 1 b1
-        pokeElemOff p 2 b2
-        pokeElemOff p 3 b3
-        pokeElemOff p 4 b4
-        pokeElemOff p 5 b5
-        pokeElemOff p 6 b6
-        pokeElemOff p 7 b7
+        pokeByteOff p 0 b0
+        pokeByteOff p 1 b1
+        pokeByteOff p 2 b2
+        pokeByteOff p 3 b3
+        pokeByteOff p 4 b4
+        pokeByteOff p 5 b5
+        pokeByteOff p 6 b6
+        pokeByteOff p 7 b7
         peek (castPtr p)
 
 {-# INLINE wordToFloat #-}
+-- |Pack 23 unspecified bits from a 'Word64' into a 'Float' in the range [0,1).
+-- Used to convert a 'stdUniform' 'Word64' to a 'stdUniform' 'Double'.
 wordToFloat :: Word64 -> Float
 wordToFloat x = (encodeFloat $! toInteger (x `shiftR` ( 41 {- 64-23 -}))) $ (-23)
 
 {-# INLINE wordToDouble #-}
+-- |Pack 52 unspecified bits from a 'Word64' into a 'Double' in the range [0,1).
+-- Used to convert a 'stdUniform' 'Word64' to a 'stdUniform' 'Double'.
 wordToDouble :: Word64 -> Double
 wordToDouble x = (encodeFloat $! toInteger (x `shiftR` ( 12 {- 64-52 -}))) $ (-52)
 
