aacddc6825f53d96b98cc825fe8a2dc809fda4b6
James Cook <james.cook@usma.edu>
2009-05-27 13:35:31 +0000

    Bernoulli CDF

--- src/Data/Random/Distribution/Bernoulli.hs
+++ src/Data/Random/Distribution/Bernoulli.hs
@@ -33,6 +33,10 @@ boolBernoulli p = do
     x <- stdUniform
     return (x <= p)
 
+boolBernoulliCDF :: (Real a) => a -> Bool -> Double
+boolBernoulliCDF p True  = 1
+boolBernoulliCDF p False = (1 - realToFrac p)
+
 -- | @generalBernoulli t f p@ generates a random variable whose value is @t@
 -- with probability @p@ and @f@ with probability @1-p@.
 generalBernoulli :: Distribution (Bernoulli b) Bool => a -> a -> b -> RVar a
@@ -40,12 +44,23 @@ generalBernoulli f t p = do
     x <- bernoulli p
     return (if x then t else f)
 
+generalBernoulliCDF :: CDF (Bernoulli b) Bool => (a -> a -> Bool) -> a -> a -> b -> a -> Double
+generalBernoulliCDF (>=) f t p x
+    | f >= t    = error "generalBernoulliCDF: f >= t"
+    | x >= t    = cdf (Bernoulli p) True
+    | x >= f    = cdf (Bernoulli p) False
+    | otherwise = 0
+
 data Bernoulli b a = Bernoulli b
 
 instance (Fractional b, Ord b, Distribution StdUniform b) 
        => Distribution (Bernoulli b) Bool
     where
         rvar (Bernoulli p) = boolBernoulli p
+instance (Distribution (Bernoulli b) Bool, Real b)
+       => CDF (Bernoulli b) Bool
+    where
+        cdf  (Bernoulli p) = boolBernoulliCDF p
 
 instance Distribution (Bernoulli b) Bool => Distribution (Bernoulli b) Int         where rvar (Bernoulli p) = generalBernoulli 0 1 p
 instance Distribution (Bernoulli b) Bool => Distribution (Bernoulli b) Int8        where rvar (Bernoulli p) = generalBernoulli 0 1 p
