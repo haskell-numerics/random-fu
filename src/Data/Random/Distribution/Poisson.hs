7ad25b437460dd8d66d0533f07ecffb18ccfbda0
James Cook <james.cook@usma.edu>
2009-05-27 14:18:01 +0000

    Poisson.hs: added CDF and cleaned up instances

--- src/Data/Random/Distribution/Poisson.hs
+++ src/Data/Random/Distribution/Poisson.hs
@@ -3,10 +3,13 @@
  -}
 {-# LANGUAGE
     MultiParamTypeClasses,
-    FlexibleInstances, FlexibleContexts, UndecidableInstances
+    FlexibleInstances, FlexibleContexts, UndecidableInstances,
+    TemplateHaskell
   #-}
 
 module Data.Random.Distribution.Poisson where
+
+import Data.Random.Internal.TH
 
 import Data.Random.Source
 import Data.Random.Distribution
@@ -47,25 +50,39 @@ integralPoisson mu = psn 0 mu
                             then prod (p * u) (k + 1)
                             else return k
 
+integralPoissonCDF :: (Integral a, Real b) => b -> a -> Double
+integralPoissonCDF mu k = exp (negate lambda) * sum
+    [ lambda ^^ i / i_fac
+    | (i, i_fac) <- zip [0..k] (scanl (*) 1 [1..])
+    ]
+    
+    where lambda = realToFrac mu
+
 fractionalPoisson :: (Num a, Distribution (Poisson b) Integer) => b -> RVar a
 fractionalPoisson mu = liftM fromInteger (poisson mu)
+
+fractionalPoissonCDF :: (CDF (Poisson b) Integer, RealFrac a) => b -> a -> Double
+fractionalPoissonCDF mu k = cdf (Poisson mu) (floor k :: Integer)
 
 poisson :: (Distribution (Poisson b) a) => b -> RVar a
 poisson mu = rvar (Poisson mu)
 
 data Poisson b a = Poisson b
 
--- so ugly...
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Int     ) b, Distribution (Binomial b) Int     ) => Distribution (Poisson b) Int        where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Int8    ) b, Distribution (Binomial b) Int8    ) => Distribution (Poisson b) Int8       where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Int16   ) b, Distribution (Binomial b) Int16   ) => Distribution (Poisson b) Int16      where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Int32   ) b, Distribution (Binomial b) Int32   ) => Distribution (Poisson b) Int32      where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Int64   ) b, Distribution (Binomial b) Int64   ) => Distribution (Poisson b) Int64      where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Word8   ) b, Distribution (Binomial b) Word8   ) => Distribution (Poisson b) Word8      where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Word16  ) b, Distribution (Binomial b) Word16  ) => Distribution (Poisson b) Word16     where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Word32  ) b, Distribution (Binomial b) Word32  ) => Distribution (Poisson b) Word32     where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Word64  ) b, Distribution (Binomial b) Word64  ) => Distribution (Poisson b) Word64     where rvar (Poisson mu) = integralPoisson mu
-instance (RealFloat b, Distribution StdUniform b, Distribution (Erlang Integer ) b, Distribution (Binomial b) Integer ) => Distribution (Poisson b) Integer    where rvar (Poisson mu) = integralPoisson mu
+$( replicateInstances ''Int integralTypes [d|
+        instance ( RealFloat b 
+                 , Distribution StdUniform   b
+                 , Distribution (Erlang Int) b
+                 , Distribution (Binomial b) Int
+                 ) => Distribution (Poisson b) Int where
+            rvar (Poisson mu) = integralPoisson mu
+        instance (Real b, Distribution (Poisson b) Int) => CDF (Poisson b) Int where
+            cdf  (Poisson mu) = integralPoissonCDF mu
+    |] )
 
-instance (Distribution (Poisson b) Integer) => Distribution (Poisson b) Float            where rvar (Poisson mu) = fractionalPoisson mu
-instance (Distribution (Poisson b) Integer) => Distribution (Poisson b) Double           where rvar (Poisson mu) = fractionalPoisson mu
+$( replicateInstances ''Float realFloatTypes [d|
+        instance (Distribution (Poisson b) Integer) => Distribution (Poisson b) Float where
+            rvar (Poisson mu) = fractionalPoisson mu
+        instance (CDF (Poisson b) Integer) => CDF (Poisson b) Float where
+            cdf  (Poisson mu) = fractionalPoissonCDF mu
+    |])
