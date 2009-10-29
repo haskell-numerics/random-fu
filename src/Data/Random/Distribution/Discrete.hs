022818d27562c9e409794c577222674db21f1e05
James Cook <james.cook@usma.edu>
2009-10-29 14:19:56 +0000

    Discrete.hs: a bunch of cleanup and such, and introduced a convenience function "empirical" for converting lists of observations into discrete distributions.

--- src/Data/Random/Distribution/Discrete.hs
+++ src/Data/Random/Distribution/Discrete.hs
@@ -13,14 +13,23 @@ import Data.Random.Distribution
 import Data.Random.Distribution.Uniform
 import Data.Random.List (randomElement)
 
+import Control.Arrow
 import Control.Monad
 import Control.Applicative
+import Data.Foldable (Foldable(foldMap))
+import Data.Traversable (Traversable(traverse, sequenceA))
 
 import Data.List
 import Data.Function
 
 discrete :: Distribution (Discrete p) a => [(p,a)] -> RVar a
 discrete ps = rvar (Discrete ps)
+
+empirical :: (Num p, Ord a) => [a] -> Discrete p a
+empirical xs = Discrete bins
+    where bins = [ (genericLength bin, x)
+                 | bin@(x:_) <- group (sort xs)
+                 ]
 
 newtype Discrete p a = Discrete [(p, a)]
     deriving (Eq, Show)
@@ -33,11 +42,21 @@ instance (Num p, Ord p, Distribution Uniform p) => Distribution (Discrete p) a w
         
         when (any (<0) ps) $ fail "negative probability in discrete distribution"
         
-        let totalProb = last cs
-        if totalProb <= 0
-            then randomElement xs   -- this probably makes the monad instance incorrect for discarding zero-probability events...
+        let totalWeight = last cs
+        if  totalWeight <= 0
+            -- if all events are "equally impossible", just pick an arbitrary one.
+            then randomElement xs
             else do
-                u <- uniform 0 totalProb
+                let getU = do
+                        u <- uniform 0 totalWeight
+                        -- reject 0; this causes integral weights to behave sensibly (although it 
+                        -- potentially wastes up to 50% of all sampled integral values in the
+                        -- case where totalWeight = 1) and is still valid for fractional weights
+                        -- (it only prevents zero-probability events from ever occurring, which 
+                        -- is reasonable).
+                        if u == 0 then getU
+                                  else return u
+                u <- getU
                 return $ head
                     [ x
                     | (c,x) <- zip cs xs
@@ -47,59 +66,71 @@ instance (Num p, Ord p, Distribution Uniform p) => Distribution (Discrete p) a w
 instance Functor (Discrete p) where
     fmap f (Discrete ds) = Discrete [(p, f x) | (p, x) <- ds]
 
+instance Foldable (Discrete p) where
+    foldMap f (Discrete ds) = foldMap (f . snd) ds
+
+instance Traversable (Discrete p) where
+    traverse f (Discrete ds) = Discrete <$> traverse (\(p,e) -> (\e -> (p,e)) <$> f e) ds
+    sequenceA  (Discrete ds) = Discrete <$> traverse (\(p,e) -> (\e -> (p,e)) <$>   e) ds
+
 -- We want each subset of cases in fx derived from a given case 
--- in x to have the same total weight as the set in x from whence they came.
---
--- thus, w(f x) == w (x) is sufficient (although not necessary), where w() is
--- the weight.
---
--- TODO: consider establishing normalization invariant?
--- TODO: Consider what should happen when f x returns multiple events with zero weight
-instance (Fractional p, Ord p) => Monad (Discrete p) where
+-- in x to have the same relative weight as the set in x from whence they came.
+instance Num p => Monad (Discrete p) where
     return x = Discrete [(1, x)]
     (Discrete x) >>= f = Discrete $ do
         (p, x) <- x
         
         let Discrete fx = f x
-        let qx = fx
-            -- TODO - check out whether this is valid when not requiring normalization...
-            -- qx = [ (q, x)
-            --      | (q, x) <- fx
-            --      , q > 0    -- should this be done?  Consider case where all results have 0 weight...
-            --      ]
-            qs = sum (map fst qx)     -- either (qx == []) or (sum qs > 0)
-            scale 
-                | qs > 0
-                = recip qs
-                | otherwise
-                = recip (fromIntegral (length qx))
+        (q, x) <- fx
         
-        (q, x) <- qx
-        
-        return (p * q * scale, x)
+        return (p * q, x)
 
-instance (Fractional p, Ord p) => Applicative (Discrete p) where
+instance Num p => Applicative (Discrete p) where
     pure = return
     (<*>) = ap
 
+-- |Like 'fmap', but for the weights of a discrete distribution.
+mapDiscreteWeights :: (p -> q) -> Discrete p e -> Discrete q e
+mapDiscreteWeights f (Discrete ds) = Discrete [(f p, x) | (p, x) <- ds]
+
+-- |Adjust all the weights of a discrete distribution so that they 
+-- sum to unity.  If not possible, returns the original distribution 
+-- unchanged.
+normalizeDiscreteWeights :: (Fractional p) => Discrete p e -> Discrete p e
+normalizeDiscreteWeights orig@(Discrete ds) = 
+    -- For practical purposes the scale factor is strict anyway,
+    -- so check if it's 0 or 1 and, if so, skip the actual scaling part.
+    if ws `elem` [0,1]
+        then orig
+        else Discrete
+                [ (w * scale, e)
+                | (w, e) <- ds
+                ] 
+    where
+        ws = sum (map fst ds)
+        scale = recip ws
+
+-- |Simplify a discrete distribution by combining equivalent events (the new
+-- event will have a weight equal to the sum of all the originals).
 collectDiscreteEvents :: (Ord e, Num p, Ord p) => Discrete p e -> Discrete p e
-collectDiscreteEvents (Discrete ds) = 
-    Discrete . concatMap (uncurry combine . unzip) . groupEvents . sortEvents $ ds
+collectDiscreteEvents = collectDiscreteEventsBy compare sum head
+        
+-- |Simplify a discrete distribution by combining equivalent events (the new
+-- event will have a weight equal to the sum of all the originals).
+-- The comparator function is used to identify events to combine.  Once chosen,
+-- the events and their weights are combined (independently) by the provided
+-- weight and event aggregation functions.
+collectDiscreteEventsBy :: (e -> e -> Ordering) -> ([p] -> p) -> ([e] -> e)-> Discrete p e -> Discrete p e
+collectDiscreteEventsBy compareE sumWeights mergeEvents (Discrete ds) = 
+    Discrete . map ((sumWeights *** mergeEvents) . unzip) . groupEvents . sortEvents $ ds
     
     where
-        groupEvents = groupBy ((==) `on` snd)
-        sortEvents  = sortBy (compare `on` snd)
-        
-        -- don't combine negative weights with positive ones.
-        -- don't error out ether - just leave them alone, it'll
-        -- all barf when the distribution is sampled.
-        combine ps (x:_) = case partition (> 0) (filter (/= 0) ps) of
-            ([], []) ->                             []
-            ([], ns) ->               (sum ns, x) : []
-            (ps, []) -> (sum ps, x) :               []
-            (ps, ns) -> (sum ps, x) : (sum ns, x) : []
+        groupEvents = groupBy (\x y -> snd x `compareE` snd y == EQ)
+        sortEvents  = sortBy (compareE `on` snd)
         
         weight (p,x)
             | p < 0     = error "negative probability in discrete distribution"
             | otherwise = p
         event ((p,x):_) = x
+        
+        combine (ps, xs) = (sumWeights ps, mergeEvents xs)
