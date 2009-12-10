830e97e9f56eca42d9473fa31731e82d605e84ba
James Cook <james.cook@usma.edu>
2009-12-10 17:46:42 +0000

    Updated to build on ghc-6.12 and bumped version to 0.0.3.1

--- src/Data/Random/Internal/Fixed.hs
+++ src/Data/Random/Internal/Fixed.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE CPP #-}
 module Data.Random.Internal.Fixed where
 
 import Data.Fixed
 import Unsafe.Coerce
+
+#ifdef base_4_2
+-- So much for backward compatibility through base (>=5) ...
+
+resolutionOf :: HasResolution r => f r -> Integer
+resolutionOf = resolution
+
+resolutionOf2 :: HasResolution r => f (g r) -> Integer
+resolutionOf2 x = resolution (res x)
+    where
+        res :: HasResolution r => f (g r) -> g r
+        res = undefined
+
+#else
 
 resolutionOf :: HasResolution r => f r -> Integer
 resolutionOf x = resolution (res x)
@@ -14,6 +29,8 @@ resolutionOf2 x = resolution (res x)
     where
         res :: HasResolution r => f (g r) -> r
         res = undefined
+
+#endif
 
 mkFixed :: Integer -> Fixed r
 mkFixed = unsafeCoerce
