cc68d85e0de328b4bae85184776ac098be3c1b31
James Cook <james.cook@usma.edu>
2010-03-23 16:31:21 +0000

    minor changes to Data.Random.Source.MWC

--- src/Data/Random/Source/MWC.hs
+++ src/Data/Random/Source/MWC.hs
@@ -15,13 +15,15 @@ import Control.Monad.ST
 instance RandomSource (ST s) (Gen s) where
     {-# INLINE supportedPrimsFrom #-}
     supportedPrimsFrom _ PrimWord8  = True
---    supportedPrimsFrom _ PrimWord32 = True
+    supportedPrimsFrom _ PrimWord16 = True
+    supportedPrimsFrom _ PrimWord32 = True
     supportedPrimsFrom _ PrimWord64 = True
     supportedPrimsFrom _ PrimDouble = True
     supportedPrimsFrom _ _ = False
     
     {-# INLINE getSupportedRandomPrimFrom #-}
     getSupportedRandomPrimFrom gen PrimWord8    = uniform gen
+    getSupportedRandomPrimFrom gen PrimWord16   = uniform gen
     getSupportedRandomPrimFrom gen PrimWord32   = uniform gen
     getSupportedRandomPrimFrom gen PrimWord64   = uniform gen
     getSupportedRandomPrimFrom gen PrimDouble   = fmap wordToDouble (uniform gen)
@@ -30,7 +32,8 @@ instance RandomSource (ST s) (Gen s) where
 instance RandomSource IO (Gen RealWorld) where
     {-# INLINE supportedPrimsFrom #-}
     supportedPrimsFrom _ PrimWord8  = True
---    supportedPrimsFrom _ PrimWord32 = True
+    supportedPrimsFrom _ PrimWord16 = True
+    supportedPrimsFrom _ PrimWord32 = True
     supportedPrimsFrom _ PrimWord64 = True
     supportedPrimsFrom _ PrimDouble = True
     supportedPrimsFrom _ _ = False
