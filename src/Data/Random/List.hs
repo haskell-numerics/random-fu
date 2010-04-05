3084090a24cf77cc895fad5d408f73a9f773d2e2
James Cook <james.cook@usma.edu>
2010-04-05 16:14:14 +0000

    Added a 'shuffleNofM' function

--- src/Data/Random/List.hs
+++ src/Data/Random/List.hs
@@ -24,15 +24,20 @@ shuffle xs = do
     
     return (SRS.shuffle xs (reverse is))
 
--- | A random variable that shuffles a list of a known length. Useful for 
--- shuffling large lists when the length is known in advance.
--- Avoids needing to traverse the list to discover its length.  Each ordering
--- has equal probability.
---
--- Throws an error the list is not exactly as long as claimed.
+-- | A random variable that shuffles a list of a known length (or a list
+-- prefix of the specified length). Useful for shuffling large lists when 
+-- the length is known in advance.  Avoids needing to traverse the list to
+-- discover its length.  Each ordering has equal probability.
 shuffleN :: Int -> [a] -> RVar [a]
-shuffleN 0 _ = return []
-shuffleN (n+1) xs = do
-    is <- sequence [uniform 0 i | i <- [n,n-1..1]]
-    return (SRS.shuffle xs is)
-shuffleN _ _ = error "shuffleN: negative length specified"
+shuffleN n xs = shuffleNofM n n xs
+
+-- | A random variable that selects N arbitrary elements of a list of known length M.
+shuffleNofM :: Int -> Int -> [a] -> RVar [a]
+shuffleNofM 0 _ _ = return []
+shuffleNofM n m xs
+    | n > m     = error "shuffleNofM: n > m"
+    | otherwise = do
+        is <- sequence [uniform 0 i | i <- take n [m-1, m-2 ..1]]
+        return (take n $ SRS.shuffle (take m xs) is)
+shuffleNofM _ _ _ = error "shuffleNofM: negative length specified"
+
