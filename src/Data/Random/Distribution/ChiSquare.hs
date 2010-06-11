42976751e0c91ba0f069578fd85c038c7054b79a
James Cook <james.cook@usma.edu>
2010-06-11 15:00:51 +0000

    Updated ChiSquare.hs to use the eventually-to-be-public 'gamma' package I'm working on.

--- src/Data/Random/Distribution/ChiSquare.hs
+++ src/Data/Random/Distribution/ChiSquare.hs
@@ -12,7 +12,7 @@ import Data.List
 
 -- This dependency needs to be extracted into a releasable package or 
 -- internal module before this module can be released.
-import NR.Ch6.S2
+import Math.Gamma
 
 chiSquare :: Distribution ChiSquare t => Int -> RVar t
 chiSquare = rvar . ChiSquare
@@ -25,4 +25,4 @@ instance (Num t, Distribution Normal t) => Distribution ChiSquare t where
     rvar (ChiSquare n) = foldl' (\xs x -> xs + x^2) 0 `fmap` replicateM n stdNormal
 
 instance (Real t, Distribution ChiSquare t) => CDF ChiSquare t where
-    cdf (ChiSquare n) x = gammp (0.5 * fromIntegral n) (0.5 * realToFrac x)
+    cdf (ChiSquare n) x = p (0.5 * fromIntegral n) (0.5 * realToFrac x)
