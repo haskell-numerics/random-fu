a0164079886f94bc753a26cb8485a040d2d904ac
james.cook <james.cook@usma.edu>
2011-05-12 12:57:26 -0400

    Updated ChiSquare module.  Still probably not quite ready for inclusion, but getting there.

--- random-fu/src/Data/Random/Distribution/ChiSquare.hs
+++ random-fu/src/Data/Random/Distribution/ChiSquare.hs
@@ -10,22 +10,21 @@ import Data.Random.Distribution.Normal
 import Control.Monad
 import Data.List
 
--- This dependency needs to be extracted into a releasable package or 
--- internal module before this module can be released.
 import Math.Gamma
 
 chiSquare :: Distribution ChiSquare t => Int -> RVar t
 chiSquare = rvar . ChiSquare
 
 chiSquareT :: Distribution ChiSquare t => Int -> RVarT m t
-chiSquareT = rvar . ChiSquare
+chiSquareT = rvarT . ChiSquare
 
 newtype ChiSquare b = ChiSquare Int
 
 -- just brute force right now.  need better implementation (inverse incomplete
 -- gamma func?).
 instance (Num t, Distribution Normal t) => Distribution ChiSquare t where
-    rvarT (ChiSquare n) = foldl' (\xs x -> xs + x^2) 0 `fmap` replicateM n stdNormalT
+    rvarT (ChiSquare n) = sumSquares `fmap` replicateM n stdNormalT
+        where sumSquares = foldl' (\xs x -> xs + x^2) 0 
 
 instance (Real t, Distribution ChiSquare t) => CDF ChiSquare t where
     cdf (ChiSquare n) x = p (0.5 * fromIntegral n) (0.5 * realToFrac x)
