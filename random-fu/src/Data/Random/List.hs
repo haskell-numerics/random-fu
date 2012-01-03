a8d853b06734e6117b68cbddfdf2428e78b02a07
James Cook <mokus@deepbondi.net>
2012-01-03 11:01:51 -0500

    Added RVarT versions of list functions

--- random-fu/src/Data/Random/List.hs
+++ random-fu/src/Data/Random/List.hs
@@ -10,17 +10,24 @@ import Control.Monad
 -- Every element has equal probability of being chosen.  Because it is a
 -- pure 'RVar' it has no memory - that is, it \"draws with replacement.\"
 randomElement :: [a] -> RVar a
-randomElement [] = error "randomElement: empty list!"
-randomElement xs = do
-    n <- uniform 0 (length xs - 1)
+randomElement = randomElementT
+
+
+randomElementT :: [a] -> RVarT m a
+randomElementT [] = error "randomElementT: empty list!"
+randomElementT xs = do
+    n <- uniformT 0 (length xs - 1)
     return (xs !! n)
 
 -- | A random variable that returns the given list in an arbitrary shuffled
 -- order.  Every ordering of the list has equal probability.
 shuffle :: [a] -> RVar [a]
-shuffle [] = return []
-shuffle xs = do
-    is <- zipWithM (\_ i -> uniform 0 i) (tail xs) [1..]
+shuffle = shuffleT
+
+shuffleT :: [a] -> RVarT m [a]
+shuffleT [] = return []
+shuffleT xs = do
+    is <- zipWithM (\_ i -> uniformT 0 i) (tail xs) [1..]
     
     return (SRS.shuffle xs (reverse is))
 
@@ -29,15 +36,21 @@ shuffle xs = do
 -- the length is known in advance.  Avoids needing to traverse the list to
 -- discover its length.  Each ordering has equal probability.
 shuffleN :: Int -> [a] -> RVar [a]
-shuffleN n xs = shuffleNofM n n xs
+shuffleN = shuffleNT
+
+shuffleNT :: Int -> [a] -> RVarT m [a]
+shuffleNT n xs = shuffleNofMT n n xs
 
 -- | A random variable that selects N arbitrary elements of a list of known length M.
 shuffleNofM :: Int -> Int -> [a] -> RVar [a]
-shuffleNofM 0 _ _ = return []
-shuffleNofM n m xs
-    | n > m     = error "shuffleNofM: n > m"
+shuffleNofM = shuffleNofMT
+
+shuffleNofMT :: Int -> Int -> [a] -> RVarT m [a]
+shuffleNofMT 0 _ _ = return []
+shuffleNofMT n m xs
+    | n > m     = error "shuffleNofMT: n > m"
     | n >= 0    = do
-        is <- sequence [uniform 0 i | i <- take n [m-1, m-2 ..1]]
+        is <- sequence [uniformT 0 i | i <- take n [m-1, m-2 ..1]]
         return (take n $ SRS.shuffle (take m xs) is)
-shuffleNofM _ _ _ = error "shuffleNofM: negative length specified"
+shuffleNofMT _ _ _ = error "shuffleNofMT: negative length specified"
 
