230875c0b321946cc79c4dc95c75f1896ceadc88
James Cook <mokus@deepbondi.net>
2012-03-22 12:27:15 -0400

    Added Pareto distribution and bumped version to 0.2.2.1

---
+++ random-fu/src/Data/Random/Distribution/Pareto.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Data.Random.Distribution.Pareto where
+
+import Data.Random
+
+pareto :: Distribution Pareto a => a -> a -> RVar a
+pareto xM a = rvar (Pareto xM a)
+
+paretoT :: Distribution Pareto a => a -> a -> RVarT m a
+paretoT xM a = rvarT (Pareto xM a)
+
+data Pareto a = Pareto !a !a
+
+instance (Floating a, Distribution StdUniform a) => Distribution Pareto a where
+    rvarT (Pareto xM a) = do
+        u <- stdUniformT
+        return (xM / (1 - u) ** recip a)
+
+instance (Real a, Distribution Pareto a) => CDF Pareto a where
+    cdf (Pareto xM a) x
+         | x >= xM      = 1 - (realToFrac xM / realToFrac x) ** realToFrac a
+         | otherwise    = 0
