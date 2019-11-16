bbd29c2416fcd6d76797b89e3284322be1740c76
Bertram Felgenhauer <int-e@gmx.de>
2019-11-16 13:47:17 +0100

    random-fu: Add support for GHC 8.8
    GHC 8.8 implements the final step in the MonadFail proposal by removing
    `fail` from the `Monad` class and instead exposing the MonadFail class.
    
    * drop `fail` from `Distribution.Categorical`'s Monad instance
    * replace `fail` for RVarT by `error` because that is the actual
      implementation on GHC < 8.8

--- random-fu/src/Data/Random/Distribution/ChiSquare.hs
+++ random-fu/src/Data/Random/Distribution/ChiSquare.hs
@@ -22,7 +22,7 @@ instance (Fractional t, Distribution Gamma t) => Distribution ChiSquare t where
     rvarT (ChiSquare 0) = return 0
     rvarT (ChiSquare n)
         | n > 0     = gammaT (0.5 * fromInteger n) 2
-        | otherwise = fail "chi-square distribution: degrees of freedom must be positive"
+        | otherwise = error "chi-square distribution: degrees of freedom must be positive"
 
 instance (Real t, Distribution ChiSquare t) => CDF ChiSquare t where
     cdf (ChiSquare n) x = incompleteGamma (0.5 * fromInteger n) (0.5 * realToFrac x)
