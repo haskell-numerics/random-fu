75d0311507fb3c8624cca3a93db2e5733348ce18
Dominic Steinitz <dominic@steinitz.org>
2020-06-27 12:23:10 +0100

    Move to random-1.2

--- release.nix
+++ release.nix
@@ -1,10 +1,27 @@
 let
-  overlay = self: super:
-{
-  random-fu     = self.haskellPackages.callPackage ./random-fu { rvar = self.rvar; };
-  random-source = self.haskellPackages.callPackage ./random-source { };
-  rvar          = self.haskellPackages.callPackage ./rvar { random-source = self.random-source; };
+  myHaskellPackageOverlay = self: super: {
+    myHaskellPackages = super.haskellPackages.override {
+    overrides = hself: hsuper: rec {
+        random = hsuper.random_1_2_0;
+        mkDerivation = args: hsuper.mkDerivation (args // {
+          doCheck = false;
+          doHaddock = false;
+        });
+
+    random-fu     = super.haskell.lib.disableLibraryProfiling (hself.callPackage  ./random-fu { });
+    random-source = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./random-source { random = hsuper.random_1_2_0; });
+    rvar          = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./rvar { });
+    splitmix      = hsuper.splitmix_0_1;
+    MonadRandom   = hsuper.MonadRandom_0_5_2;
+    };
+  };
 };
+
+  nixpkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable-2018-06-28";
+    url = "https://github.com/nixos/nixpkgs/archive/056b0df2b614893b15dd696e74d093f6c58590a0.tar.gz";
+    sha256 = "1zjrjk5pfzl1nsii90q1v43cf55jr66igapa8pqgxdg465p186c2";
+}) { overlays = [ myHaskellPackageOverlay ]; };
 
 in
 
@@ -12,4 +29,3 @@ nixpkgs.myHaskellPackages.callPackage ./tests/speed {
   random-source = nixpkgs.myHaskellPackages.random-source;
   random-fu     = nixpkgs.myHaskellPackages.random-fu;
 }
-
