4016399d4aa14103c07f08288529979740b37cec
Dominic Steinitz <dominic@steinitz.org>
2020-06-27 12:23:10 +0100

    Move to random-1.2

--- release.nix
+++ release.nix
@@ -5,6 +5,17 @@ let
     random-fu     = super.haskell.lib.disableLibraryProfiling (hself.callPackage  ./random-fu { });
     random-source = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./random-source { });
     rvar          = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./rvar { });
+
+    # random =
+    #     let newRandomSrc = builtins.fetchGit {
+    #       url = "https://github.com/idontgetoutmuch/random.git";
+    #       rev = "8ac2c89c8394555f56ade4eda34051599833e885";
+    #       ref = "v1.2-proposal";
+    #       };
+    #         ran = hself.callCabal2nix "random" newRandomSrc {};
+    #       in
+    #       super.haskell.lib.dontCheck ran;
+
       };
     };
   };
@@ -16,5 +27,7 @@ in
 nixpkgs.myHaskellPackages.callPackage ./tests/speed {
   random-source = nixpkgs.myHaskellPackages.random-source;
   random-fu     = nixpkgs.myHaskellPackages.random-fu;
+  # rvar          = nixpkgs.myHaskellPackages.rvar;
+  # random        = nixpkgs.myHaskellPackages.random;
 }
 
