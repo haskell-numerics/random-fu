7e4d588ba0b8dc8ef5d5a99c8640f279a3be4982
Dominic Steinitz <dominic@steinitz.org>
2020-07-01 05:47:06 +0100

    Really fix CI

--- release.nix
+++ release.nix
@@ -17,7 +17,11 @@ let
   };
 };
 
-  nixpkgs = import <nixpkgs> { overlays = [ myHaskellPackageOverlay ]; };
+  nixpkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable-2018-06-28";
+    url = "https://github.com/nixos/nixpkgs/archive/056b0df2b614893b15dd696e74d093f6c58590a0.tar.gz";
+    sha256 = "1zjrjk5pfzl1nsii90q1v43cf55jr66igapa8pqgxdg465p186c2";
+}) { overlays = [ myHaskellPackageOverlay ]; };
 
 in
 
