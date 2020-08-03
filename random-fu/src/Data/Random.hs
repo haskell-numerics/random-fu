a6193fd0a44c0fa28ba04727037dd703b41d70a3
Dominic Steinitz <dominic@steinitz.org>
2020-08-03 10:12:12 +0100

    Revert "Merge pull request #58 from haskell-numerics/bench-build"
    This reverts commit ec873e257157de0bdd8d3895ab054dd692cc2cc5, reversing
    changes made to 022c3cba84bc4aa7df00b90429649b1389626ea6.

--- random-fu/src/Data/Random.hs
+++ random-fu/src/Data/Random.hs
@@ -67,6 +67,7 @@ import Data.Random.Source (MonadRandom, RandomSource)
 import Data.Random.Source.IO ()
 import Data.Random.Source.MWC ()
 import Data.Random.Source.StdGen ()
+import Data.Random.Source.PureMT ()
 import Data.Random.Source.Std
 import Data.Random.Distribution
 import Data.Random.Distribution.Gamma
