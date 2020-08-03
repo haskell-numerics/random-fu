cabd6cee14613924f0c5c11c8196a42c775cdbce
Dominic Steinitz <dominic@steinitz.org>
2020-08-03 10:11:09 +0100

    Revert "Merge pull request #60 from haskell-numerics/random-1.2"
    This reverts commit 3de77f71f4cf507317145db36a0c86aef0371903, reversing
    changes made to 2d2c31eadd619403b78a8d5db34d8fcf15d01dd9.

--- release.nix
+++ release.nix
@@ -2,26 +2,14 @@ let
   myHaskellPackageOverlay = self: super: {
     myHaskellPackages = super.haskellPackages.override {
     overrides = hself: hsuper: rec {
-        random = hsuper.random_1_2_0;
-        mkDerivation = args: hsuper.mkDerivation (args // {
-          doCheck = false;
-          doHaddock = false;
-        });
-
     random-fu     = super.haskell.lib.disableLibraryProfiling (hself.callPackage  ./random-fu { });
-    random-source = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./random-source { random = hsuper.random_1_2_0; });
+    random-source = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./random-source { });
     rvar          = super.haskell.lib.disableLibraryProfiling (hself.callPackage ./rvar { });
-    splitmix      = hsuper.splitmix_0_1;
-    MonadRandom   = hsuper.MonadRandom_0_5_2;
+      };
     };
   };
-};
 
-  nixpkgs = import (builtins.fetchTarball {
-    name = "nixos-unstable-2018-06-28";
-    url = "https://github.com/nixos/nixpkgs/archive/056b0df2b614893b15dd696e74d093f6c58590a0.tar.gz";
-    sha256 = "1zjrjk5pfzl1nsii90q1v43cf55jr66igapa8pqgxdg465p186c2";
-}) { overlays = [ myHaskellPackageOverlay ]; };
+  nixpkgs = import <nixpkgs> { overlays = [ myHaskellPackageOverlay ]; };
 
 in
 
@@ -29,3 +17,4 @@ nixpkgs.myHaskellPackages.callPackage ./tests/speed {
   random-source = nixpkgs.myHaskellPackages.random-source;
   random-fu     = nixpkgs.myHaskellPackages.random-fu;
 }
+
